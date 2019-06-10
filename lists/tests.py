c0cc7b5331f1b81317be01b0bafb18d94a19fbeb
nconlisk <noecon@gmail.com>
2019-06-10 15:58:59 +0100

    Update to home, views, unit tests
    Update to home, views, unit tests

--- lists/tests.py
+++ lists/tests.py
@@ -14,3 +14,6 @@ class HomePageTest(TestCase):
 
     #    Rather than testing hard coded 'constants' this now tests our implementation.
 
+    def test_can_save_a_POST_request(self):
+        response =  self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
